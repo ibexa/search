7966db21e87801e4d0e83772d509fef65a1d5ea8
Krzysztof Słomka <kisztof@users.noreply.github.com>
2023-10-11 12:45:48 +0200

    Removed root location

--- src/lib/EventDispatcher/EventListener/BreadCrumbSubscriber.php
+++ src/lib/EventDispatcher/EventListener/BreadCrumbSubscriber.php
@@ -7,15 +7,17 @@ declare(strict_types=1);
 
 namespace Ibexa\Search\EventDispatcher\EventListener;
 
+use Ibexa\Contracts\Core\Repository\LocationService;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 use Ibexa\Search\EventDispatcher\Event\PostAutoCompleteSearch;
 use Ibexa\Search\Model\Suggestion;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 final class BreadCrumbSubscriber implements EventSubscriberInterface
 {
-    private \Ibexa\Contracts\Core\Repository\LocationService $locationService;
+    private LocationService $locationService;
 
-    public function __construct(\Ibexa\Contracts\Core\Repository\LocationService $locationService)
+    public function __construct(LocationService $locationService)
     {
         $this->locationService = $locationService;
     }
@@ -30,12 +32,11 @@ final class BreadCrumbSubscriber implements EventSubscriberInterface
     public function onPostAutoCompleteSearch(PostAutoCompleteSearch $event): PostAutoCompleteSearch
     {
         /** @var Suggestion $suggestion */
-        foreach ($event->getSuggestionCollection() as $suggestion){
-            foreach ($suggestion->getParentsLocation() as $locationId => $name){
+        foreach ($event->getSuggestionCollection() as $suggestion) {
+            foreach ($suggestion->getParentsLocation() as $locationId => $name) {
                 $location = $this->locationService->loadLocation($locationId);
                 $suggestion->addPath($locationId, $location->contentInfo->name);
             }
-
         }
 
         return $event;
--- src/lib/Mapper/SearchResultToSuggestionCollectionMapper.php
+++ src/lib/Mapper/SearchResultToSuggestionCollectionMapper.php
@@ -8,25 +8,40 @@ declare(strict_types=1);
 namespace Ibexa\Search\Mapper;
 
 use Ibexa\Contracts\Core\Repository\Values\Content\Search\SearchResult;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 use Ibexa\Search\Model\Suggestion;
 use Ibexa\Search\Model\SuggestionCollection;
 
-class SearchResultToSuggestionCollectionMapper
+final class SearchResultToSuggestionCollectionMapper
 {
+    private ConfigResolverInterface $configResolver;
+
+    public function __construct(ConfigResolverInterface $configResolver)
+    {
+        $this->configResolver = $configResolver;
+    }
+
     public function transform(SearchResult $searchResult, ?string $language = null): SuggestionCollection
     {
+        $rootLocationId = $this->configResolver->getParameter('content.tree_root.location_id');
         $collection = [];
         /** @var \Ibexa\Core\Repository\Values\Content\Content $result */
         foreach ($searchResult as $result) {
             /** @var \Ibexa\Core\Repository\Values\Content\Content $content */
             $content = $result->valueObject;
 
+            $parentsLocation = $result->valueObject->contentInfo->mainLocation->path;
+            $position = array_search((string)$rootLocationId, $parentsLocation);
+            if ($position !== false) {
+                $parentsLocation = array_slice($parentsLocation, $position);
+            }
+
             $collection[] = new Suggestion(
                 $result->valueObject->contentInfo->id,
                 $content->getFieldValue('title', $language)->text,
                 $result->valueObject->contentInfo->getContentType()->identifier,
-                $result->valueObject->contentInfo->mainLocation->pathString,
-                array_flip($result->valueObject->contentInfo->mainLocation->path)
+                implode('/', $parentsLocation),
+                array_flip($parentsLocation)
             );
         }
 
