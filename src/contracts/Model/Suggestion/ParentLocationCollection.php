248cede26abb77b5185a8ad4ca32cdfbd39043a9
Krzysztof Słomka <kisztof@gmail.com>
2023-11-07 14:03:15 +0100

    Update src/contracts/Model/Suggestion/ParentLocationCollection.php
    Co-authored-by: Paweł Niedzielski <pawel.niedzielski@ibexa.co>

--- src/contracts/Model/Suggestion/ParentLocationCollection.php
+++ src/contracts/Model/Suggestion/ParentLocationCollection.php
@@ -27,8 +27,8 @@ final class ParentLocationCollection extends MutableArrayList
             throw new InvalidArgumentException(
                 '$item',
                 sprintf(
-                    'Argument 1 passed to %s::append() must be an instance of %s, %s given',
-                    __CLASS__,
+                    'Argument 1 passed to %s() must be an instance of %s, %s given',
+                    __METHOD__,
                     Suggestion::class,
                     is_object($item) ? get_class($item) : gettype($item)
                 )
