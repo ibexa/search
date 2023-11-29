ea2ca5b6e0ccb08896c90c4d473964d5687ce5d9
Krzysztof Słomka <kisztof@users.noreply.github.com>
2023-11-29 09:54:32 +0100

    Fixed min search query lenght in autocomplete

--- src/bundle/Controller/SuggestionController.php
+++ src/bundle/Controller/SuggestionController.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Ibexa\Bundle\Search\Controller;
 
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 use Ibexa\Search\Model\SuggestionQuery;
 use Ibexa\Search\Service\SuggestionService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,14 +18,23 @@ final class SuggestionController extends AbstractController
 {
     private SuggestionService $suggestionService;
 
+    private ConfigResolverInterface $configResolver;
+
     public function __construct(
-        SuggestionService $suggestionService
+        SuggestionService $suggestionService,
+        ConfigResolverInterface $configResolver
     ) {
         $this->suggestionService = $suggestionService;
+        $this->configResolver = $configResolver;
     }
 
     public function suggestAction(SuggestionQuery $suggestionQuery): JsonResponse
     {
+        $minQueryLength = $this->configResolver->getParameter('search.suggestion.min_query_length');
+        if (strlen($suggestionQuery->getQuery()) < $minQueryLength) {
+            return $this->json([]);
+        }
+
         $result = $this->suggestionService->suggest($suggestionQuery);
 
         return $this->json($result);
